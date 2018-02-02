cfeb32ec8ee0bd4715fc78e5c467794a54cf50fa
Zsolt Tomis <tzsolt@sensity.com>
2018-02-02 13:23:24 +0100

    Bugfix for VB2 state handling KEVA-1128

--- drivers/altera-pci/eutecus-altera-pci.h
+++ drivers/altera-pci/eutecus-altera-pci.h
@@ -203,6 +203,34 @@ inline static int eutecus_remap(struct vm_area_struct * vma, resource_size_t sta
     return io_remap_pfn_range(vma, vma->vm_start, start >> PAGE_SHIFT, vma->vm_end - vma->vm_start, vma->vm_page_prot);
 }
 
+static const char * vb_state_names[] = {
+	"VB2_BUF_STATE_DEQUEUED",
+	"VB2_BUF_STATE_PREPARING",
+	"VB2_BUF_STATE_PREPARED",
+	"VB2_BUF_STATE_QUEUED",
+	"VB2_BUF_STATE_REQUEUEING",
+	"VB2_BUF_STATE_ACTIVE",
+	"VB2_BUF_STATE_DONE",
+	"VB2_BUF_STATE_ERROR",
+};
+
+static inline const char * get_vb_state_name(struct vb2_buffer * vb)
+{
+    int state;
+
+    if(!vb){
+        return "null";
+    }
+
+    state = vb->state;
+
+    if (state < 0 || state > VB2_BUF_STATE_ERROR) {
+        return "unknown";
+    }
+
+    return vb_state_names[state];
+}
+
 int init_cfile(struct eutecus_pci_data * data, struct pci_dev * dev);
 void destroy_cfile(struct eutecus_pci_data * data);
 int altera_v4l2_initialize(struct pci_dev * dev);
