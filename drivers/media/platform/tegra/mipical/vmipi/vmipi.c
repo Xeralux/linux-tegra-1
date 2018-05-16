72635728bbb7e898802a48ba5eabdbb45d494620
Matt Madison <matthew.madison@verizon.com>
2018-05-16 08:12:22 -0700

    media: tegra: mipical: add missing include

--- drivers/media/platform/tegra/mipical/vmipi/vmipi.c
+++ drivers/media/platform/tegra/mipical/vmipi/vmipi.c
@@ -20,6 +20,7 @@
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 #include <linux/mutex.h>
+#include <linux/sched.h>
 #include <linux/semaphore.h>
 #include <linux/wait.h>
 #include <linux/tegra-ivc.h>
